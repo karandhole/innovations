5775e0f6609ca0227a370331331fcbc78b19a2cd
karan dhole <karan.dhole20@pccoepune.org.>
2025-03-11 12:09:54 +0530

    first update

--- src/common/seo/Seo.js
+++ src/common/seo/Seo.js
@@ -4,10 +4,10 @@ const SEO = ({ pageTitle }) => (
     <>
         <Head>
             <title>
-                {pageTitle && `${pageTitle} || Seacab || Creative Agency React Next JS Template`}
+                {pageTitle && `${pageTitle} ||Kaiteki Innovations `}
             </title>
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-            <meta name="description" content="Seacab Creative Agency React Next JS Template" />
+            <meta name="description" content="Kaiteki Innovations " />
             <meta name="robots" content="noindex, follow" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         </Head>
