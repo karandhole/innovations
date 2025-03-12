c9b4abbf4d63273b99cece49094d0e8684fbc7e3
karan dhole <karan.dhole20@pccoepune.org.>
2025-03-12 11:26:15 +0530

    S

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,13 +21,13 @@ export default function AboutPage() {
                 currentPage="About" 
             />
 			<AboutTwo />
-			<CounterOne />
-			<TeamOne />
+			 <CounterOne />
+			{/*<TeamOne />
 			<ServiceOne />
 			<BrandOne />
 			<ContactTwo />
 			<VideoOne />
-			<BlogThree />
+			<BlogThree /> */}
 			<FooterOne />
 		</>
 	);
